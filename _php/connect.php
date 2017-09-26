037a4071dbddb2c482dd22228f1262c3e92bab3e
Phillip Attard <s3472625@student.rmit.edu.au>
2017-09-26 12:50:10 +0000

    fix

--- _php/connect.php
+++ _php/connect.php
@@ -1,11 +1,11 @@
 <?php
-echo("<p>Connect is running</p>");
-$host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
-$port=3306;
-$socket="";
-$user="proProg";
-$password="pawprogramming";
-$dbname="pawCompanion";
-$connection = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+    print("Connect is running");
+    $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $password="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $password, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
 ?>
