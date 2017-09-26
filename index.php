3ab7ae6b8936648ac9060fdf18b28a6e98537549
Phillip Attard <s3472625@student.rmit.edu.au>
2017-09-26 12:52:51 +0000

    update

--- index.php
+++ index.php
@@ -15,16 +15,8 @@
         git push heroku master
 -->
 
-<?php
-$host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
-$port=3306;
-$socket="";
-$user="proProg";
-$password="pawprogramming";
-$dbname="pawCompanion";
-$connection = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-?>
+<!-- Connect AWS MYSQL Server -->
+<?php include_once('_php/connect.php');?>
 
 
 <?php
