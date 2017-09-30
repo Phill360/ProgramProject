a04d0d3df86e8a07b3b64a8b2262f93852dbc5a7
Phillip Attard <s3472625@student.rmit.edu.au>
2017-09-30 05:59:19 +0000

    Adding image upload functionality

--- admin.php
+++ admin.php
@@ -86,34 +86,31 @@
             <br>
             
             
-            <div class="imageupload panel panel-default">
-    <div class="panel-heading clearfix">
-        <h3 class="panel-title pull-left">Upload Image</h3>
-        <div class="btn-group pull-right">
-            <button type="button" class="btn btn-default active">File</button>
-            <button type="button" class="btn btn-default">URL</button>
-        </div>
-    </div>
-    <div class="file-tab panel-body">
-        <label class="btn btn-default btn-file">
-            <span>Browse</span>
-            <!-- The file is stored here. -->
-            <input type="file" name="image-file">
-        </label>
-        <button type="button" class="btn btn-default">Remove</button>
-    </div>
-    <div class="url-tab panel-body">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Image URL">
-            <div class="input-group-btn">
-                <button type="button" class="btn btn-default">Submit</button>
+            <div>
+            <h1 class="page-header">Bootstrap Image Upload Form</h1>
+            <p class="lead">Select a PNG or JPEG image, having maximum size <span id="max-size"></span> KB.</p>
+
+            <form id="upload-image-form" action="" method="post" enctype="multipart/form-data">
+            <div id="image-preview-div" style="display: none">
+            <label for="exampleInputFile">Selected image:</label>
+            <br>
+            <img id="preview-img" src="noimage">
             </div>
-        </div>
-        <button type="button" class="btn btn-default">Remove</button>
-        <!-- The URL is stored here. -->
-        <input type="hidden" name="image-url">
-    </div>
-</div>
+            <div class="form-group">
+            <input type="file" name="file" id="file" required>
+            </div>
+            <button class="btn btn-lg btn-primary" id="upload-button" type="submit" disabled>Upload image</button>
+            </form>
+            <br>
+            <div class="alert alert-info" id="loading" style="display: none;" role="alert">
+            Uploading image...
+            <div class="progress">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+            </div>
+            </div>
+            </div>
+            <div id="message"></div>
+            </div>
             
 
             <div class="form-group">
