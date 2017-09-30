39fb6b89aa66c6cdc32e7238b2d00acc99c61957
Phillip Attard <s3472625@student.rmit.edu.au>
2017-09-30 05:27:43 +0000

    Add image upload functionality

--- admin.php
+++ admin.php
@@ -85,7 +85,36 @@
             </div>
             <br>
             
-            <input id="input-b1" name="input-b1" type="file" class="file">
+            
+            <div class="imageupload panel panel-default">
+    <div class="panel-heading clearfix">
+        <h3 class="panel-title pull-left">Upload Image</h3>
+        <div class="btn-group pull-right">
+            <button type="button" class="btn btn-default active">File</button>
+            <button type="button" class="btn btn-default">URL</button>
+        </div>
+    </div>
+    <div class="file-tab panel-body">
+        <label class="btn btn-default btn-file">
+            <span>Browse</span>
+            <!-- The file is stored here. -->
+            <input type="file" name="image-file">
+        </label>
+        <button type="button" class="btn btn-default">Remove</button>
+    </div>
+    <div class="url-tab panel-body">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Image URL">
+            <div class="input-group-btn">
+                <button type="button" class="btn btn-default">Submit</button>
+            </div>
+        </div>
+        <button type="button" class="btn btn-default">Remove</button>
+        <!-- The URL is stored here. -->
+        <input type="hidden" name="image-url">
+    </div>
+</div>
+            
 
             <div class="form-group">
               <label for="age">Gender:</label>
