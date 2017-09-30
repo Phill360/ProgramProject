06c37d5bb65b15f7bd75cae4d8ebd46472d09b40
Phillip Attard <s3472625@student.rmit.edu.au>
2017-09-30 04:23:32 +0000

    Fix font issue

--- admin.php
+++ admin.php
@@ -7,9 +7,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-
   <link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="pcstyle.css">
 </head>
 
