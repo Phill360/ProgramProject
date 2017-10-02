72affc082aa953928026f187a324ac9fa3b21441
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-02 05:03:19 +0000

    Working on landing page design

--- landing.php
+++ landing.php
@@ -25,7 +25,7 @@
 
   <div class="jumbotron">
     <div class="h1_slackey"><div class="textHuge">Paw Companions</div></div>
-    <div class="h2_opensans"><div class="textxxLarge">Find your true companion..</div></div>
+    <div class="h2_opensans"><div class="textxxLarge tocenter">Find your true companion..</div></div>
     <div class="btn-toolbar">
       <button type="button" class="btn btn-primary pull-right">Sign in</button>
       <button type="button" class="btn btn-primary pull-right">Register</button>
