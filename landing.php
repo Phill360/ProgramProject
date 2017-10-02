8b05f38987c135fe63e8cbcb8021a20b571132c3
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-02 05:49:27 +0000

    COMMENT ON CHANGES THAT WERE MADE

--- landing.php
+++ landing.php
@@ -61,6 +61,26 @@
       </div>
     </div>
   </div>
+  
+  <!-- Sign In Modal -->
+  <div id="registerModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <div class="modal-title"><div class="slackey"><div class="textLarge">Register</div></div></div>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
   
   
