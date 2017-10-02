c4689c82ed4d38634397154e035133ee6e4f464b
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-02 07:29:21 +0000

    COMMENT ON CHANGES THAT WERE MADE

--- admin.php
+++ admin.php
@@ -20,10 +20,10 @@
   <script>
   $(document).ready(function(){
     $("#signOutBtn").on("click", function(){
-      alert("Clicked the sign out button")
       var auth2 = gapi.auth2.getAuthInstance();
       auth2.signOut().then(function () {
         console.log('User signed out.');
+        alert("User signed out");
       });
     });
   });
