69fb97b7c1795cbf78055c9238bb555da69f1958
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-02 05:47:02 +0000

    COMMENT ON CHANGES THAT WERE MADE

--- landing.php
+++ landing.php
@@ -41,28 +41,29 @@
       </div>
     </div>
   </div>
-</div>
+  
+  <!-- Sign In Modal -->
+  <div id="signInModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
 
-
-<!-- Sign In Modal -->
-<div id="signInModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <div class="modal-title"><div class="slackey"><div class="textLarge">Sign in</div></div></div>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <div class="modal-title"><div class="slackey"><div class="textLarge">Sign in</div></div></div>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
-
   </div>
+</div>
+  
+  
 </div>
 
 </body>
