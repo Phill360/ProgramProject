3f25773f294c9b2b2fcaa4211c69d9448774a7c2
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-02 03:55:44 +0000

    Working on responsive font size

--- landing.php
+++ landing.php
@@ -24,7 +24,7 @@
 <div class="container">
 
   <div class="jumbotron">
-    <div class="h1_slackey">Paw Companions</div>
+    <h1 class="display-1">Paw Companions</div>
     <div class="btn-toolbar">
       <button type="button" class="btn btn-primary pull-right">Sign in</button>
       <button type="button" class="btn btn-primary pull-right">Register</button>
