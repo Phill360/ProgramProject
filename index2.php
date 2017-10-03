f5291328b067c58d6b9206797882ec72fc5fd60b
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-03 04:03:58 +0000

    Get google profile

--- index2.php
+++ index2.php
@@ -72,6 +72,16 @@
   <meta name="google-signin-client_id" content="979917733927-ucaoh1mmkqkmpp8oqfnonj45fjdcd7n4.apps.googleusercontent.com">
   
   <script>
+  function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  </script>
+  
+  <script>
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
@@ -79,7 +89,6 @@
     });
   }
   </script>
-  
   
 </head>
 
