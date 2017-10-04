14df7e5a1a961099ebe4f905330f96de92c3273d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-04 13:38:03 +0000

    db

--- common.php
+++ common.php
@@ -42,21 +42,7 @@
       $_SESSION['usertype'] = $usertype;
       header('Location: index2.php');
     }
-    
-    
-    
-    
-    $users2 = array();
-    
-    while ( !feof($fp) )
-    {
-      $line = fgets($fp, 2048);
-      $data = str_getcsv($line, $delimiter);
-      array_push($users, $data);
-    }  
 
-    $size = sizeof($users2);
-    
     fclose($fp);
     return $size;
   }
