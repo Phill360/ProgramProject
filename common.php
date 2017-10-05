110f879f06d032f8e1ea94a3b6347fedce0781c1
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-05 00:27:36 +0000

    db

--- common.php
+++ common.php
@@ -190,7 +190,7 @@
     $fp = fopen($file, 'a+');
     for ($row = 0; $row < $size; $row++) 
     {
-      fwrite($fp, $users[$row][0]);
+      fwrite($fp, $users[$row][0].PHP_EOL);
       //fwrite($fp, $users[$row][0].','.$users[$row][1].','.$users[$row][2].','.$users[$row][3].','.$users[$row][4].','.$users[$row][5].PHP_EOL);
     }
     fclose($fp);
