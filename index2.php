6eb7e2b5478f3e601eaed920cebad748b6317edf
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-05 07:16:19 +0000

    validation

--- index2.php
+++ index2.php
@@ -205,5 +205,28 @@
     </div>
   </div>
 </div>
+
+
+<script>
+$(document).ready(function() {
+    $('#registerForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            email: {
+                validators: {
+                    emailAddress: {
+                        message: 'The value is not a valid email address'
+                    }
+                }
+            }
+        }
+    });
+});
+</script>
 </body>
 </html>
