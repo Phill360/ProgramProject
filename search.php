52efadada1446c5f146a82176a1df9f45fd325e1
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-08 10:42:45 +0000

    test

--- search.php
+++ search.php
@@ -11,7 +11,7 @@
     <div class="col-sm-12">
       <div class="panel panel-default">
         <div class="panel-body">
-          <h1>This will be the search page..</h1>
+          <h1>This will be the questionnaire page..</h1>
         </div>
       </div>
     </div>
