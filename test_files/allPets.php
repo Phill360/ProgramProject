35c018816fdee6f6cf64da65cf51464d876b04c6
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-09 12:52:50 +0000

    all_pets_test

---
+++ test_files/allPets.php
@@ -0,0 +1,107 @@
+<!-- Heroku Website address 
+    https://stark-citadel-72039.herokuapp.com/
+
+    **** Commands for updating git and pushing to Heroku ****
+    Add all files to git
+        git add .
+    
+    Commit files to GitHub
+        git commit -am " COMMENT ON CHANGES THAT WERE MADE "
+
+    Push all files to GitHub
+        git push
+
+    Push all files to Heroku to update site 
+        git push heroku master
+-->
+
+<!-- Connect AWS MYSQL Server -->
+<?php include_once('_php/connect.php');?>
+
+
+<?php
+	// 2. Perform Query
+	$query = "SELECT * ";
+	$query .= "FROM animals ";
+	$result = mysqli_query($connection, $query);
+	// Test for query error
+	if(!$result) {
+		die("Database query failed.");
+	}
+?>
+
+
+
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+	<head>
+		<title>AWS Test___</title>
+	</head>
+
+
+	<style>
+
+	table {
+		width: 100%;
+	}
+
+	table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+	}
+	</style>
+
+
+	<body>
+		<h1> Paw Companion Test </h1>
+		
+		
+		<p>Phill</p>
+		<p>Git Test</p>
+		<p>Mark - Yay, this worked! </p>
+		<p>Bash Script Worked! - Updated again</p>
+		<a href="index2.php">Site page</a>
+		
+		
+		
+		<h2>User Table<h2>
+		<table >
+			<tr>
+				<th>rspacaID</th>
+				<th>petName</th>
+				<th>breedID</th>
+				<th>gender</th>
+				<th>desexed</th>
+				<th>vaccinated</th>
+				<th>wormed</th>
+				<th>heartworm</th>
+				<th>imagepath</th>
+			</tr>
+			<?php
+				// 3. returned data
+				while($row = mysqli_fetch_assoc($result)) {
+			?>
+			<tr>
+				<td><?php echo $row["rspacaID"] ; ?> </td>
+				<td><?php echo $row["petName"] ; ?>	</td>
+				<td><?php echo $row["breedID"] ; ?>	</td>
+					<td><?php echo $row["gender"] ; ?> </td>
+				<td><?php echo $row["desexed"] ; ?>	</td>
+				<td><?php echo $row["vaccinated"] ; ?>	</td>
+					<td><?php echo $row["wormed"] ; ?> </td>
+				<td><?php echo $row["heartworm"] ; ?>	</td>
+				<td><?php echo $row["imagepath"] ; ?>	</td>
+			</tr>
+			<?php
+				}
+			?>
+			
+		</table>
+	</body>
+</html>
+
+
+<?php
+	// 5. Close database connection
+	mysqli_close($connection);
+?>
