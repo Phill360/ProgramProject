ce6de04957accf02a81a163e0af67843f80d75ab
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-09 10:38:36 +0000

    debugging

--- common.php
+++ common.php
@@ -44,7 +44,7 @@
       // Secure password string
    	  $userpass = md5($password);
    	  
-      $query = mysql_query("INSERT INTO user (userID, first_name, last_name, email, password, admin) VALUES ('0', $firstname, $lastname, $email, $password, $usertype)");
+      $query = mysqli_query("INSERT INTO user (userID, first_name, last_name, email, password, admin) VALUES ('0', $firstname, $lastname, $email, $password, $usertype)");
 
       if ($connection->query($query) === TRUE) 
       {
