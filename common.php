e5c102ee28c2f239aa9f2c169e3e515d4599b5ac
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-09 07:32:38 +0000

    debugging

--- common.php
+++ common.php
@@ -12,7 +12,93 @@
   /* This function registers a user */
   function registerUser($firstname, $lastname, $email, $password, $visits, $usertype)
   {
-    //
+    $delimiter = ',';
+    $file = 'users.txt';
+    $fp = fopen($file, 'r');
+    $users = array();
+    
+    while ( !feof($fp) )
+    {
+      $line = fgets($fp, 2048);
+      $data = str_getcsv($line, $delimiter);
+      array_push($users, $data);
+    }  
+
+    $size = sizeof($users);
+    
+    for ($row = 0; $row < $size; $row++) 
+    {
+      if ($users[$row][0] == $email)
+      {
+        $check = 'previous user exists';
+      }
+      else
+      {
+        $check = 'no previous user';
+      }
+    }
+    fclose($fp);
+    
+    $fp = fopen($file, 'a+');
+    
+    if ($check == 'no previous user')
+    {
+      // Secure password string
+   	  $userpass = md5($password);
+   	  fwrite($fp, PHP_EOL.$email.','.$userpass.','.$lastname.','.$firstname.','.$visits.','.$usertype);
+   	  $_SESSION['validUser'] = true;
+      $_SESSION['usertype'] = $usertype;
+      header('Location: index2.php');
+    }
+    fclose($fp);
+  }
+
+  /* This function signs the user in */
+  function signInUser($email, $password)
+  {
+    // Check user existance	
+    $file = 'users.txt';
+    $fp = fopen($file, "r");
+    $users = array();
+    
+    while ( !feof($fp) )
+    {
+      $line = fgets($fp, 2048);
+      $data = str_getcsv($line, $delimiter);
+      array_push($users, $data);
+    }  
+
+    $size = sizeof($users);
+    for ($row = 0; $row < $size; $row++) 
+    {
+      if ($users[$row][0] == $email)
+      {
+        // User exists, now check the password.
+        if ($users[$row][1] == md5($password))
+   	    {
+   	      $validUser = true;
+   	      $usertype = $users[$row][5];
+   	    }
+      }
+    }
+    fclose($fp);
+    
+    if ($email == 'super' && $password == 'super')
+    {
+      $validUser = true;
+      $usertype = "admin";
+    }
+    
+    if ($validUser == true) 
+    {
+      $_SESSION['validUser'] = true;
+      $_SESSION['usertype'] = $usertype;
+      header('Location: index2.php');	  
+    }
+    else
+    {
+      $_SESSION['validUser'] = false;
+    }
   }
 
   
