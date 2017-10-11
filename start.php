4aafccd00d0d8d92d44d461dc87a306bb543170f
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-11 09:15:54 +0000

    ui

--- start.php
+++ start.php
@@ -51,8 +51,8 @@
     </header>
     
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/popper/popper.min.js"></script>
+    <!--<script src="vendor/jquery/jquery.min.js"></script>-->
+    <!--<script src="vendor/popper/popper.min.js"></script>-->
     <!--<script src="vendor/bootstrap/js/bootstrap.min.js"></script>-->
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
