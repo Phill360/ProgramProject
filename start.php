cdffc65b8fdfc38007225bf3de3f721b7ff14c06
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-11 09:38:49 +0000

    update

--- start.php
+++ start.php
@@ -57,6 +57,8 @@
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/popper/popper.min.js"></script>
     <!--<script src="vendor/bootstrap/js/bootstrap.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
     
     
 </html>
