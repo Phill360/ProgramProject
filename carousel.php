43c1acb6f2a71f2b0f6c437244d4b5d3a60e435b
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-12 02:16:09 +0000

    ui

--- carousel.php
+++ carousel.php
@@ -48,9 +48,6 @@
     <div class="col-sm-12">
       <!-- Create new admin user box -->
       <div class="panel panel-default">
-        <div class="panel-heading">
-          <div class="opensans">Promote normal user to admin user</div>
-        </div>
         <div class="panel-body">
           <a href="admin_user.php">Admin</a>
         </div>
