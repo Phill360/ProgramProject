ac71f0e3f3f646451324517acf4c72e997ff7bd0
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-12 13:10:00 +0000

    db

--- admin.php
+++ admin.php
@@ -13,7 +13,15 @@ include_once('./common.php');
 <div class="container">
 <div class="slackey"><div class="black"><div class="textxxMedium">Welcome admin user</div></div></div>
 
-  
+<div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="promote_user.php">Promote</a></li>
+    <li><a href="demote_user.php">Demote</a></li>
+    <li><a href="pet_add.php">Add a pet</a></li>
+  </ul>
+</div>  
   
   
   
