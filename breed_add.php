7e925bd57e73a11bc016bd4fc3fb90e70ebe6529
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-12 12:50:17 +0000

    db

---
+++ breed_add.php
@@ -0,0 +1,79 @@
+<?php
+require_once('./functions.php');
+?>
+
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+
+</head>
+
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <!-- Add breed -->
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="opensans">Add a breed</div>
+        </div>
+        <div class="panel-body">
+          <form action="./test_files/add_breed.php" method="post">
+            
+            <!-- Select cat or dog -->
+            <div class="form-group">
+              <label for="type">Species:</label>
+              <select class="form-control" id="species" name="species">
+                <option>Cat</option>
+                <option>Dog</option>
+              </select>
+            </div>
+            
+                    <!-- Enter breed name -->
+            <div class="input-group">
+              <span class="input-group-addon">Breed</span>
+              <input id="petName" type="text" class="form-control" name="breedName" placeholder="Enter breed name">
+            </div>
+            <br>
+
+            <!-- Enter breed size -->
+            <div class="input-group">
+              <span class="input-group-addon">Text</span>
+             <select class="form-control" id="breedSize" name="breedSize">
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+              </select>
+            </div>
+            <br>
+
+            <!-- Enter breed temperament -->
+            <div class="input-group">
+              <span class="input-group-addon">Temperament</span>
+               <select class="form-control" id="temperament" name="temperament">
+                <option>Placid</option>
+                <option>Friendly</option>
+                <option>Not sure</option>
+              </select>
+            </div>
+            <br>
+            
+            <!-- Enter breed active -->
+            <div class="input-group">
+              <span class="input-group-addon">Active</span>
+              <select class="form-control" id="active" name="active">
+                <option>Quite</option>
+                <option>Active</option>
+                <option>Very Active</option>
+              </select>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
