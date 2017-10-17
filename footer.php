3b1458e22f90d2d6ae3fc6b4ef6264dba73c5fad
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-17 03:37:10 +0000

    add footer

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <div class="container">
+    <div class="col-sm-12">
+      <!-- Footer box -->
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <p>Footer section</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
