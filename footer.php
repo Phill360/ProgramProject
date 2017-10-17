1954127f6a8cd8bb72fe5da6ac3ff8ab069e869c
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-17 03:55:00 +0000

    update

--- footer.php
+++ footer.php
@@ -9,12 +9,14 @@
 </head>
 
 <body>
-<div class="container"
+<div><footer class="fixed-bottom">
+<div class="container">
   <div class="panel panel-default">
     <div class="panel-body"></div>
     <div class="panel-footer">Copyright © 2017 Paw Companions. All rights reserved.</div>
   </div>
-</div>  
+</div>
+</div>
 </body>
 
 </html>
