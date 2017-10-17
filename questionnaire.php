caf543684f0472b055f9f6b5b308d719f27b1cf8
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-17 09:01:25 +0000

    update

--- questionnaire.php
+++ questionnaire.php
@@ -25,19 +25,21 @@
 <body>
   <div class="container">
     <div class="slackey"><div class="black"><div class="textxxMedium">Welcome admin user</div></div></div>
-    <div id="slider-step"></div>
+    <br>
+    <div class="opensans"><div class="black"><div class="textRegular">How many adults in the household?</div></div></div>
+    <div id="slider-step-numberAdults"></div>
   </div>
 </body>
 
 <script>
-    var stepSlider = document.getElementById('slider-step');
+    var stepSlider = document.getElementById('slider-step-numberAdults');
 
 noUiSlider.create(stepSlider, {
-	start: [ 4000 ],
-	step: 1000,
+	start: [ 0 ],
+	step: 1,
 	range: {
-		'min': [  2000 ],
-		'max': [ 10000 ]
+		'min': [ 0 ],
+		'max': [ 3 ]
 	}
     });
     
@@ -49,42 +51,5 @@ noUiSlider.create(stepSlider, {
 </script>
 
 
-<script>
-    var range = document.getElementById('range');
 
-    noUiSlider.create(range, {
-        start: [ 20, 80 ], // Handle start position
-        step: 10, // Slider moves in increments of '10'
-        margin: 20, // Handles must be more than '20' apart
-        connect: true, // Display a colored bar between the handles
-        direction: 'rtl', // Put '0' at the bottom of the slider
-        orientation: 'vertical', // Orient the slider vertically
-        behaviour: 'tap-drag', // Move handle on tap, bar is draggable
-        range: { // Slider can select '0' to '100'
-            'min': 0,
-            'max': 100
-        },
-        pips: { // Show a scale with the slider
-            mode: 'steps',
-            density: 2
-        }
-    });
-
-    var valueInput = document.getElementById('value-input'),
-            valueSpan = document.getElementById('value-span');
-
-    // When the slider value changes, update the input and span
-    range.noUiSlider.on('update', function( values, handle ) {
-        if ( handle ) {
-            valueInput.value = values[handle];
-        } else {
-            valueSpan.innerHTML = values[handle];
-        }
-    });
-
-    // When the input changes, set the slider value
-    valueInput.addEventListener('change', function(){
-        range.noUiSlider.set([null, this.value]);
-    });
-</script>
 </html>
