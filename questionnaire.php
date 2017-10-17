7e2984a6d3d80d778c903b3639511827c7cf4dcc
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-17 08:19:13 +0000

    update

--- questionnaire.php
+++ questionnaire.php
@@ -23,8 +23,30 @@
     </style>
 </head>
 <body>
+    <div id="slider-step"></div>
     <div id="range"></div>
 </body>
+
+<script>
+    var stepSlider = document.getElementById('slider-step');
+
+noUiSlider.create(stepSlider, {
+	start: [ 4000 ],
+	step: 1000,
+	range: {
+		'min': [  2000 ],
+		'max': [ 10000 ]
+	}
+    });
+    
+    var stepSliderValueElement = document.getElementById('slider-step-value');
+
+    stepSlider.noUiSlider.on('update', function( values, handle ) {
+	stepSliderValueElement.innerHTML = values[handle];
+    });
+</script>
+
+
 <script>
     var range = document.getElementById('range');
 
