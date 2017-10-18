1165d857caa1296fdc14a22b8b3eecb2367c268c
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-18 11:11:29 +0000

    db

---
+++ terms.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+  <title>Terms & Conditions</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Google services -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <meta name="google-signin-client_id" content="979917733927-ucaoh1mmkqkmpp8oqfnonj45fjdcd7n4.apps.googleusercontent.com">
+  
+  <!-- Bootstrap JS -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="upload-image.js"></script>
+
+  <!-- Google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Slackey" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  
+  <!-- Pet Companions CSS -->
+  <link rel="stylesheet" type="text/css" href="css/pcstyle.css">
+  
+</head>
+<body>
+
+<div><?php include 'header.php' ?></div>
+ 
+<div class="container">
+  
+  <h1>Terms & Conditions</h1>
+  <h2>Use of this site and the information available on this site is subject to 
+  the following terms and conditions:</h2>
+  <h2>1. Ownership</h2>
+  <p>This site (“Website”) is owned and operated by Paw Companions.</p>
+  <h2>2. Terminology</h2>
+  <p>In these terms and conditions, the expressions “we”, “us”, and “our” are 
+  references to Paw Companions.</p>
+  <h2>3. Use of Website from outside Australia</h2>
+  <p>The information on this Website (“the Information”) and the Terms and 
+  Conditions have been prepared in accordance with Australian law. If you are 
+  residing in or accessing this Website from a country other than Australia 
+  (“the User Country”) the Information and the Terms and Conditions may not 
+  satisfy the laws of the User Country. Should the Information and the Terms and 
+   Conditions satisfy the laws of the User Country, then you are entitled to use 
+  the Website. In the event that the Information or the Terms and Conditions do 
+  not satisfy the laws of the User Country, this Website is not intended for your 
+  use, and you are not entitled to rely on the Information. If you do so, you agree 
+  to indemnify us for any loss or damage which we may incur as a consequence.</p>
+
+4. Acceptance of Terms and Conditions
+
+4.1 Your access and use of the Website is conditional upon your acceptance and compliance with the terms, conditions, notices and disclaimers contained in this document and elsewhere on the Website (known collectively as the “Terms and Conditions”). Your use of and/or continued access to the Website constitutes your agreement to the Terms and Conditions.
+
+4.2 We may amend the Terms and Conditions at any time by posting the amended terms and conditions on the Website. The amended terms and conditions will be effective immediately and you will be bound by the amended terms and conditions from that time. This agreement may not be amended otherwise. You should visit this page periodically to review the Terms and Conditions. If you violate any Terms and Conditions your right to use and access the Website automatically terminates.
+
+5. Restrictions on Use
+The Website is available only for your personal and non-profit use. We have the right to change or discontinue any feature of the Website including the Material defined in clause 6, hours of availability and equipment required for access. You may make a single hard copy of the Material. You may not otherwise copy, reproduce, republish, frame, post, upload, distribute, transmit or modify in any way all or any part of the material contained on this Website, unless expressly provided for on the Website or expressly authorised in writing by RSPCA. You must not transmit or attempt to transmit any data, code or other material of any kind to this Website which contains a virus or other harmful component.
+
+6. Trade marks
+All names, logos and trademarks are either our property or the property of third parties who have contributed to the Website. Nothing on the Website should be interpreted as granting any rights to use or distribute any names, logos or trademarks, without our express written agreement or the relevant contributor. Nothing displayed on the Website should be construed as granting any licence or right to use any name, logo or trademark without our express permission, or the relevant third party contributor.
+
+7.Copyright
+The content of this Website, including all information such as text, graphics, images and other material (“Material”), is protected by Australian and international copyright law. You may download a single copy of the Material and where necessary for reference purposes keep a temporary copy in your computer’s cache and make a single hard copy of the Material. You may make such other use of the material as is otherwise expressly authorised on the Website. Unauthorised use of the Material may violate these copyright laws.
+
+8. No Representations
+We make no representations about the accuracy, reliability, completeness or timeliness of the Material. The Material may contain inaccuracies or typographical errors. The use of the Website and the Material is at your own risk. Changes are periodically made to the Website and may be made at any time.
+
+9. No Warranties
+9.1 The Website and Material are provided on an as is basis without any warranties of any kind. We disclaim all warranties to the fullest extent permitted by law.
+
+9.2 The Trade Practices Act 1974 (Cth) and all corresponding state and territory legislation implies terms, conditions and warranties into some contracts for the supply of goods and services and prohibits the exclusion, restriction and modification of such terms (“Prescribed Terms”). Except as provided by the Prescribed Terms, all warranties express or implied by law in any way relating to access to, or non access to, the Website or the use of or reliance upon the Website or the Material are excluded.
+
+9.3 In addition, we do not warrant that the Website will operate error free or that this Website and its servers are free of computer viruses and other harmful data, code, components or other material. Nor do we warrant that it will be able to prevent any illegal, harmful or inappropriate use, modification or alteration of the Website, or will give notice of such use, modification or alteration. If your use of the Website or the Material results in the need for servicing or replacing equipment or data, we are not responsible for those costs.
+
+10. Limitation of Liability
+Except as provided by the Prescribed Terms, neither we, our suppliers, or any third parties mentioned on the Website shall be liable for any loss or damage whatsoever (including, without limitation, incidental and consequential damages, lost profits, or damages) resulting from the use or access to or inability to use and access the Website and the Material.
+
+11. Specific Warnings
+You must ensure that your access to this Website is not illegal or prohibited by laws which apply to you. You must take your own precautions to ensure that the process which you employ for accessing this Website does not expose you to the risk of viruses, malicious computer code or other forms of interference which may damage your own computer system.
+
+12. Taxes
+Certain taxes and government charges may be payable in relation to the use of our services. We have no responsibility for such taxes and other government charges on transactions on or in anyway connected with the Website. However should any charge be made by us for the supply of goods or services on the Website, such charges are inclusive of any goods and services tax imposed under Australian law (“GST”).
+
+13. Internet Service Providers
+If you are an internet service and/or access provider, which supplies the material to your subscriber, you must not use the Website in any manner or for any purpose which is unlawful or in any manner which violates any of our rights or which is prohibited by the Terms and Conditions.
+
+14. Hyperlinks
+14.1 The Website contains hyperlinks and other pointers to internet websites operated by third parties (“Linked Sites”). The Linked Sites are not under our control and we are not responsible for the content of any Linked Site or any hyperlink contained in a Linked Site.
+
+14.2 The inclusion of any link does not imply our endorsement of the Linked Site. You link to any such Linked Site entirely at your own risk. We exercise no control over the quality, safety or legality of the items advertised or sold and statements made through any Linked Sites.
+
+14.3 The Material on any Linked Site, including product information and prices, are the responsibility of the operator of the Linked Site. Any information contained on a Linked Site is subject to change without notice by the operator of that website. We are not liable for the prices or price changes, including where price changes have not been reflected on the relevant site.
+
+14.4 Any purchases or dealings you have with a Linked Site are done at your own risk. We are not a party to any transaction between you and a Linked Site. Your use of a Linked Site is subject to the terms and conditions of that site in addition to the Terms and Conditions of this Website. If there is any inconsistency, to the extent of the inconsistency, the Terms and Conditions of this Website prevail.
+
+15. Advertisements
+
+15.1 The Website display of the Price List is an advertisement made by the Vendor RSPCAs.
+
+15.2 The Website may contain embedded hyperlinks or referral buttons to websites operated by third parties or their licensees or contractors (Advertisers). Clause 14 also applies to sites operated by Advertisers. Any claims by Advertisers are not recommendations or endorsements by us.
+
+16. Disputes with Linked Sites and Advertisers
+As we do not and cannot be involved in your interaction with Linked Sites and Advertisers, in the event that you have a dispute with one or more Linked Sites or Advertisers, to the extent permitted by law, you agree to release us (and our agents and employees) from liability for any claims, demands and damages (actual and consequential) arising out of or in any way connected with such disputes.
+
+17. Termination
+The Terms and Conditions are effective until terminated by us. We may terminate this agreement and your access to the Website at any time without notice. ln the event of termination, you are no longer authorised to access the Website. All restrictions imposed on you, disclaimers and limitations of liability set out in the Terms and Conditions will survive termination.
+
+18. Security of Information
+Unfortunately, no data transmission over the Internet can be guaranteed as totally secure. Whilst we strive to protect such information, we do not warrant and cannot ensure the security of any information which you transmit to us. Accordingly, any information which you transmit to us is transmitted at your own risk. Nevertheless, once we receive your transmission, we will take reasonable steps to preserve the security of such information.
+
+19.Indemnity Clause
+You indemnify us, regardless of any negligence on our part, against all losses, liabilities, legal costs and other expenses incurred by us arising directly or indirectly as a result of or in connections with the breach by you of any provision of these Terms and Conditions or any wilful, unlawful or negligent act by you in connection with the supply by a Vendor RSPCA to you of, or use by you of, any Product.
+
+20. Miscellaneous
+If any provision of the Terms and Conditions is found to be invalid or unenforceable by a court of law, such invalidity or unenforceability will not affect the remainder of the Terms and Conditions which will continue in full force and effect. All rights not expressly granted are reserved. This agreement sets out the entire understanding and agreement between RSPCA and you with respect to the subject matter.
+
+Use of this website is conditional upon your acceptance and compliance with certain Terms and Conditions.
+  <p>This part is inside a .container class.</p> 
+  <p>The .container class provides a responsive fixed width container.</p>           
+</div>
+
+<div><?php include 'footer.php' ?></div>
+
+
+</body>
+</html>
