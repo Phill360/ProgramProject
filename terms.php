271abde40ac3d955edf6ac25ad384b855b2c6606
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-18 11:14:40 +0000

    db

--- terms.php
+++ terms.php
@@ -51,46 +51,99 @@
   not satisfy the laws of the User Country, this Website is not intended for your 
   use, and you are not entitled to rely on the Information. If you do so, you agree 
   to indemnify us for any loss or damage which we may incur as a consequence.</p>
-
-4. Acceptance of Terms and Conditions
-
-4.1 Your access and use of the Website is conditional upon your acceptance and compliance with the terms, conditions, notices and disclaimers contained in this document and elsewhere on the Website (known collectively as the “Terms and Conditions”). Your use of and/or continued access to the Website constitutes your agreement to the Terms and Conditions.
-
-4.2 We may amend the Terms and Conditions at any time by posting the amended terms and conditions on the Website. The amended terms and conditions will be effective immediately and you will be bound by the amended terms and conditions from that time. This agreement may not be amended otherwise. You should visit this page periodically to review the Terms and Conditions. If you violate any Terms and Conditions your right to use and access the Website automatically terminates.
-
-5. Restrictions on Use
-The Website is available only for your personal and non-profit use. We have the right to change or discontinue any feature of the Website including the Material defined in clause 6, hours of availability and equipment required for access. You may make a single hard copy of the Material. You may not otherwise copy, reproduce, republish, frame, post, upload, distribute, transmit or modify in any way all or any part of the material contained on this Website, unless expressly provided for on the Website or expressly authorised in writing by RSPCA. You must not transmit or attempt to transmit any data, code or other material of any kind to this Website which contains a virus or other harmful component.
-
-6. Trade marks
-All names, logos and trademarks are either our property or the property of third parties who have contributed to the Website. Nothing on the Website should be interpreted as granting any rights to use or distribute any names, logos or trademarks, without our express written agreement or the relevant contributor. Nothing displayed on the Website should be construed as granting any licence or right to use any name, logo or trademark without our express permission, or the relevant third party contributor.
-
-7.Copyright
-The content of this Website, including all information such as text, graphics, images and other material (“Material”), is protected by Australian and international copyright law. You may download a single copy of the Material and where necessary for reference purposes keep a temporary copy in your computer’s cache and make a single hard copy of the Material. You may make such other use of the material as is otherwise expressly authorised on the Website. Unauthorised use of the Material may violate these copyright laws.
-
-8. No Representations
-We make no representations about the accuracy, reliability, completeness or timeliness of the Material. The Material may contain inaccuracies or typographical errors. The use of the Website and the Material is at your own risk. Changes are periodically made to the Website and may be made at any time.
-
-9. No Warranties
-9.1 The Website and Material are provided on an as is basis without any warranties of any kind. We disclaim all warranties to the fullest extent permitted by law.
-
-9.2 The Trade Practices Act 1974 (Cth) and all corresponding state and territory legislation implies terms, conditions and warranties into some contracts for the supply of goods and services and prohibits the exclusion, restriction and modification of such terms (“Prescribed Terms”). Except as provided by the Prescribed Terms, all warranties express or implied by law in any way relating to access to, or non access to, the Website or the use of or reliance upon the Website or the Material are excluded.
-
-9.3 In addition, we do not warrant that the Website will operate error free or that this Website and its servers are free of computer viruses and other harmful data, code, components or other material. Nor do we warrant that it will be able to prevent any illegal, harmful or inappropriate use, modification or alteration of the Website, or will give notice of such use, modification or alteration. If your use of the Website or the Material results in the need for servicing or replacing equipment or data, we are not responsible for those costs.
-
-10. Limitation of Liability
-Except as provided by the Prescribed Terms, neither we, our suppliers, or any third parties mentioned on the Website shall be liable for any loss or damage whatsoever (including, without limitation, incidental and consequential damages, lost profits, or damages) resulting from the use or access to or inability to use and access the Website and the Material.
-
-11. Specific Warnings
-You must ensure that your access to this Website is not illegal or prohibited by laws which apply to you. You must take your own precautions to ensure that the process which you employ for accessing this Website does not expose you to the risk of viruses, malicious computer code or other forms of interference which may damage your own computer system.
-
-12. Taxes
-Certain taxes and government charges may be payable in relation to the use of our services. We have no responsibility for such taxes and other government charges on transactions on or in anyway connected with the Website. However should any charge be made by us for the supply of goods or services on the Website, such charges are inclusive of any goods and services tax imposed under Australian law (“GST”).
-
-13. Internet Service Providers
-If you are an internet service and/or access provider, which supplies the material to your subscriber, you must not use the Website in any manner or for any purpose which is unlawful or in any manner which violates any of our rights or which is prohibited by the Terms and Conditions.
-
-14. Hyperlinks
-14.1 The Website contains hyperlinks and other pointers to internet websites operated by third parties (“Linked Sites”). The Linked Sites are not under our control and we are not responsible for the content of any Linked Site or any hyperlink contained in a Linked Site.
+  <h2>4. Acceptance of Terms and Conditions</h2>
+  <h2>4.1</h2><p>Your access and use of the Website is conditional upon your 
+  acceptance and compliance with the terms, conditions, notices and disclaimers 
+  contained in this document and elsewhere on the Website (known collectively as 
+  the “Terms and Conditions”). Your use of and/or continued access to the Website 
+  constitutes your agreement to the Terms and Conditions.</p>
+  <h2>4.2</h2><p>We may amend the Terms and Conditions at any time by posting 
+  the amended terms and conditions on the Website. The amended terms and conditions 
+  will be effective immediately and you will be bound by the amended terms and 
+  conditions from that time. This agreement may not be amended otherwise. You 
+  should visit this page periodically to review the Terms and Conditions. If you 
+  violate any Terms and Conditions your right to use and access the Website 
+  automatically terminates.</p>
+  <h2>5. Restrictions on Use</h2>
+  <p>The Website is available only for your personal and non-profit use. We have 
+  the right to change or discontinue any feature of the Website including the 
+  Material defined in clause 6, hours of availability and equipment required for 
+  access. You may make a single hard copy of the Material. You may not otherwise 
+  copy, reproduce, republish, frame, post, upload, distribute, transmit or modify 
+  in any way all or any part of the material contained on this Website, unless 
+  expressly provided for on the Website or expressly authorised in writing by Paw 
+  Companions. You must not transmit or attempt to transmit any data, code or 
+  other material of any kind to this Website which contains a virus or other 
+  harmful component.</p>
+  <h2>6. Trade marks</h2>
+  <p>All names, logos and trademarks are either our property or the property of 
+  third parties who have contributed to the Website. Nothing on the Website 
+  should be interpreted as granting any rights to use or distribute any names, 
+  logos or trademarks, without our express written agreement or the relevant 
+  contributor. Nothing displayed on the Website should be construed as granting 
+  any licence or right to use any name, logo or trademark without our express 
+  permission, or the relevant third party contributor.</p>
+  <h2>7.Copyright</h2>
+  <p>The content of this Website, including all information such as text, 
+  graphics, images and other material (“Material”), is protected by Australian 
+  and international copyright law. You may download a single copy of the Material 
+  and where necessary for reference purposes keep a temporary copy in your 
+  computer’s cache and make a single hard copy of the Material. You may make 
+  such other use of the material as is otherwise expressly authorised on the 
+  Website. Unauthorised use of the Material may violate these copyright laws.</p>
+  <h2>8. No Representations</h2>
+  <p>We make no representations about the accuracy, reliability, completeness or 
+  timeliness of the Material. The Material may contain inaccuracies or 
+  typographical errors. The use of the Website and the Material is at your own 
+  risk. Changes are periodically made to the Website and may be made at any time.</p>
+  <h2>9. No Warranties</h2>
+  <h2>9.1</h2><p>The Website and Material are provided on an as is basis without 
+  any warranties of any kind. We disclaim all warranties to the fullest extent 
+  permitted by law.</p>
+  <h2>9.2</h2><p>The Trade Practices Act 1974 (Cth) and all corresponding state 
+  and territory legislation implies terms, conditions and warranties into some 
+  contracts for the supply of goods and services and prohibits the exclusion, 
+  restriction and modification of such terms (“Prescribed Terms”). Except as
+  provided by the Prescribed Terms, all warranties express or implied by law in 
+  any way relating to access to, or non access to, the Website or the use of or 
+  reliance upon the Website or the Material are excluded.</p>
+  <h2>9.3</h2><p>In addition, we do not warrant that the Website will operate 
+  error free or that this Website and its servers are free of computer viruses 
+  and other harmful data, code, components or other material. Nor do we warrant 
+  that it will be able to prevent any illegal, harmful or inappropriate use, 
+  modification or alteration of the Website, or will give notice of such use, 
+  modification or alteration. If your use of the Website or the Material results 
+  in the need for servicing or replacing equipment or data, we are not responsible 
+  for those costs.</p>
+  <h2>10. Limitation of Liability</h2>
+  <p>Except as provided by the Prescribed Terms, neither we, our suppliers, or 
+  any third parties mentioned on the Website shall be liable for any loss or 
+  damage whatsoever (including, without limitation, incidental and consequential 
+  damages, lost profits, or damages) resulting from the use or access to or 
+  inability to use and access the Website and the Material.</p>
+  <h2>11. Specific Warnings</h2>
+  <p>You must ensure that your access to this Website is not illegal or prohibited 
+  by laws which apply to you. You must take your own precautions to ensure that 
+  the process which you employ for accessing this Website does not expose you to 
+  the risk of viruses, malicious computer code or other forms of interference 
+  which may damage your own computer system.</p>
+  <h2>12. Taxes</h2>
+  <p>Certain taxes and government charges may be payable in relation to the use 
+  of our services. We have no responsibility for such taxes and other government 
+  charges on transactions on or in anyway connected with the Website. However 
+  should any charge be made by us for the supply of goods or services on the 
+  Website, such charges are inclusive of any goods and services tax imposed under 
+  Australian law (“GST”).</p>
+  <h2>13. Internet Service Providers</h2>
+  <p>If you are an internet service and/or access provider, which supplies the 
+  material to your subscriber, you must not use the Website in any manner or for 
+  any purpose which is unlawful or in any manner which violates any of our rights 
+  or which is prohibited by the Terms and Conditions.</p>
+  <h2>14. Hyperlinks</h2>
+  <h2>14.1</h2><p>The Website contains hyperlinks and other pointers to internet 
+  websites operated by third parties (“Linked Sites”). The Linked Sites are not 
+  under our control and we are not responsible for the content of any Linked 
+  Site or any hyperlink contained in a Linked Site.</p>
 
 14.2 The inclusion of any link does not imply our endorsement of the Linked Site. You link to any such Linked Site entirely at your own risk. We exercise no control over the quality, safety or legality of the items advertised or sold and statements made through any Linked Sites.
 
