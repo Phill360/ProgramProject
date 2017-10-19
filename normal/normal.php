2df899e470d2a9bc76f2f32286e1366ba07735eb
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-19 00:59:56 +0000

    db

--- normal/normal.php
+++ normal/normal.php
@@ -1,22 +1,5 @@
 <?php
-  include_once('./common.php');
 
-  
-
-  if(isset($_POST['questionnaireBtn']))
-  {
-    $userTool = 'questionnaire';
-  }
-
-  if(isset($_POST['matchesBtn']))
-  {
-    $userTool = 'matches';
-  }
-
-  if(isset($_POST['favouritesBtn']))
-  {
-    $userTool = 'favourites';
-  }
 ?>
 
 <!DOCTYPE html PUBLIC>
