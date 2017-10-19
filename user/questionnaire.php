77b4df0d806846192f62c6c8de0677a8171a3a68
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-19 22:29:27 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -39,6 +39,22 @@
 </body>
 
 <script>
+  var sliderFormat = document.getElementById('slider-format');
+
+noUiSlider.create(sliderFormat, {
+	start: [ 20000 ],
+	step: 1000,
+	range: {
+		'min': [ 20000 ],
+		'max': [ 80000 ]
+	},
+	format: wNumb({
+		decimals: 3,
+		thousand: '.',
+		postfix: ' (US $)',
+	})
+});
+  
   var inputFormat = document.getElementById('input-format');
 
   sliderFormat.noUiSlider.on('update', function( values, handle ) {
