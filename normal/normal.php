10b19140c59f686b9034f39adcaf59cfb99cd752
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-19 00:58:28 +0000

    db

--- normal/normal.php
+++ normal/normal.php
@@ -27,32 +27,7 @@
 <body>
 <div class="container">
 	<h1>This is a test page.</h1>
-  <div class="slackey"><div class="black"><div class="textxxMedium">Welcome</div></div></div>
-    <div>
-      <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" id="userToolSelectionForm">
-        <button type="submit" class="btn" name="questionnaireBtn">Questionnaire</button>
-        <button type="submit" class="btn" name="matchesBtn">Matches</button>
-        <button type="submit" class="btn" name="favouritesBtn">Favourites</button>
-      </form>
-    </div>
 
-  <?php 
-    if ($userTool == 'questionnaire')
-    {?>
-      <div><?php include 'questionnaire.php' ?></div>
-    <?php }
-    else if ($userTool == 'matches')
-    {?>
-      <div><?php include 'matches.php' ?></div>
-  <?php } 
-    else if ($userTool == 'favourites')
-    {?>
-      <div><?php include 'favourites.php' ?></div>
-  <?php } 
-    else
-    {?>
-      <div><?php include 'matches.php' ?></div>
-  <?php } ?>
 </div>
 
 </body>
