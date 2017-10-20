8b27491442d491960a369d3e0f1af9493c49ddc6
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 10:57:21 +0000

    db

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -2,30 +2,20 @@
 <html>
 <head>
   <link href="css/pcstyle.css" rel="stylesheet" />
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 
 <body>
   <p>Test</p>
   
-  <div id="slidecontainer">
-    <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-  </div>
+ 
   
   <rzslider rz-slider-model="slider.value"
           rz-slider-options="slider.options"></rzslider>
 
 
 
-  <script>
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-    output.innerHTML = this.value;
-    }
-  </script>
+  
   
   <script>
       $scope.slider = {
