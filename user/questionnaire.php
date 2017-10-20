b2b74a9911d0d39aa4c7b2189f1fdcc89c11187c
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 07:57:03 +0000

    db

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script>
+        $("#ex18a").slider({
+	min: 0,
+	max: 10,
+	value: 5,
+	labelledby: 'ex18-label-1'
+});
+$("#ex18b").slider({
+	min: 0,
+	max: 10,
+	value: [3, 6],
+	labelledby: ['ex18-label-2a', 'ex18-label-2b']
+});
+    </script>
+    
 </head>
 
 <body>
