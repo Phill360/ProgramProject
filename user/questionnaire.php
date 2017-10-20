9f143a48fda0e89b2e085a857553d475b22b323d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 08:43:28 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -8,33 +8,13 @@
     <link rel="stylesheet" type="text/css" href="bootstrap-slider.min.css">
     
     <script>
-    // With JQuery
-$('#ex1').slider({
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
-
-// Without JQuery
-var slider = new Slider('#ex1', {
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
+    var mySlider = $("input.slider").bootstrapSlider();
+    
     </script>
     
 </head>
 
 <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="opensans"><div class="black"><div class="textRegular">1. How many adults in the household?</div></div></div>
-          <br>
-             <input id="ex1" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="14"/>
-          <br>
-        </div>
-    </div>
-  </div>
+    <input id="input.slider" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="14"/>
 </body>
 </html>
