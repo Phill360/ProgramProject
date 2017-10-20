4e7316746ced782fab2f6e88b6d1b17abddcbfde
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 10:08:55 +0000

    db

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -22,15 +22,17 @@
 
     <script>
         // Instantiate a slider
-var mySlider = $("input.slider").bootstrapSlider();
+var mySlider = new Slider("input.slider", {
+    // initial options object
+});
  
 // Call a method on the slider
-var value = mySlider.bootstrapSlider('getValue');
+var value = mySlider.getValue();
  
 // For non-getter methods, you can chain together commands
-    mySlider
-        .bootstrapSlider('setValue', 5)
-        .bootstrapSlider('setValue', 7);
+mySlider
+    .setValue(5)
+    .setValue(7);
     </script>
     
 </head>
