34c01ad9dea5a0b2f8b33dfacda14f9e5ac9e5a8
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 00:47:13 +0000

    db

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <link href="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.js"></script>
+<link href="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.min.css" rel="stylesheet">
+<script src="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/6.2.0/jquery.nouislider.min.css">
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<!-- <script src="https://raw.githubusercontent.com/leongersen/libLink/master/jquery.liblink.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/6.2.0/jquery.nouislider.min.js"></script>
 
 
 <style>
-    #slider {
+#slider {
   margin: 20px;
   width: 70%;
 }
@@ -27,8 +26,8 @@
         <div class="col-sm-6">
           <div class="opensans"><div class="black"><div class="textRegular">1. How many adults in the household?</div></div></div>
           <br>
-          
-            <input type="text" class="unibox-price-min" placeholder="1" onfocus="uniboxResetHint('Min Price',false,this);" onblur="uniboxResetHint('Min Price',true,this);" value="Min Price" onkeyup="uniboxKeyUp(event,this)" onkeydown="uniboxKeyDown(event,this)"
+            <div class="unibox-search unibox-search-regular">
+              <input type="text" class="unibox-price-min" placeholder="1" onfocus="uniboxResetHint('Min Price',false,this);" onblur="uniboxResetHint('Min Price',true,this);" value="Min Price" onkeyup="uniboxKeyUp(event,this)" onkeydown="uniboxKeyDown(event,this)"
   />
 
             <div id="slider"></div>
@@ -37,18 +36,13 @@
 <div class="unibox-quick-summary-line">
   <span class="unibox-quick-summary"></span><span>&nbsp;</span> 
 </div>
-<!--<input type="hidden" value="recip" class="unibox-search-context" name="recip">-->
-<!--<input type="hidden" value="1" class="unibox-search-region" name="34">-->
-<!--<input type="hidden" value="29619" class="unibox-search-account" name="26743">-->
-<!--<input type="hidden" value="http://firsthomeplan.ca/propertylistings" class="unibox-search-result-page" name="/fairrealty/listings/">-->
-<!--<input type="hidden" value="AUTO" class="unibox-search-listing-type" name="AUTO">-->
 </div>
 
           <br>
         </div>
     </div>
   </div>
-<div class="unibox-search unibox-search-regular">
+
 
   
   
