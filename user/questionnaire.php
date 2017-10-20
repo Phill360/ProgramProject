81048089e39709c64fc35403f43eb686793e21d5
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-20 07:59:46 +0000

    db

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -25,15 +25,7 @@ $("#ex18b").slider({
           <div class="opensans"><div class="black"><div class="textRegular">1. How many adults in the household?</div></div></div>
           <br>
              <span id="ex18-label-1" class="hidden">Example slider label</span>
-             <input id="ex19" type="text"
-              data-provide="slider"
-              data-slider-ticks="[1, 2, 3]"
-              data-slider-ticks-labels='["short", "medium", "long"]'
-              data-slider-min="1"
-              data-slider-max="3"
-              data-slider-step="1"
-              data-slider-value="3"
-              data-slider-tooltip="hide" />
+<input id="ex18a" type="text"/>
           <br>
         </div>
     </div>
