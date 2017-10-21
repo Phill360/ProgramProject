a083d5e703cfdeddde52fd2235be369dfc449e91
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-21 06:57:38 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -1,3 +1,12 @@
+<?php
+  /* Collect data from sliders  */
+  $postdata = file_get_contents("php://input");
+  $request = json_decode($postdata);
+  @$a1 = $request->a1;
+  @$a2 = $request->a2;
+  echo($a1);
+?>
+
 <!DOCTYPE html PUBLIC>
 <html lang="en">
 <head>
@@ -16,12 +25,13 @@
 <div ng-app="rzSliderDemo">
   <div ng-controller="MainCtrl" >
 
-
     <h2>Slider with ticks and values</h2>
     <rzslider rz-slider-model="slider_ticks_values.value" rz-slider-options="slider_ticks_values.options"></rzslider>
 
     <h2>Range slider with ticks and values</h2>
     <rzslider rz-slider-model="range_slider_ticks_values.minValue" rz-slider-high="range_slider_ticks_values.maxValue" rz-slider-options="range_slider_ticks_values.options"></rzslider>
+    
+    <button ng-click="sendValues()">Submit</button><br><span id="message"></span>
   </div>
 </div>
 
@@ -52,6 +62,24 @@ app.controller('MainCtrl', function ($scope, $rootScope, $timeout, $modal)
       showTicksValues: true
     }
   };
+  
+  $scope.sendValues = function () {
+    document.getElementById("message").textContent = "";
+
+    var request = $http({
+      method: "post",
+      url: window.location.href + "$_SERVER['PHP_SELF']",
+      data: {
+        a1: $scope.slider_ticks_values,
+        a2: $scope.range_slider_ticks_values
+      },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    
+    request.success(function(data) {
+      document.getElementById("message").textContent = "You have successfully sent values "+data;  
+    });
+  };  
 });
 </script>
 
