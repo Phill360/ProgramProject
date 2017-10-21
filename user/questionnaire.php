fdd65d998ffde63d5dd766cae82a9ede8a97b710
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-21 11:33:31 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -25,7 +25,8 @@
   <div class="slider">
     <h2>Slider with ticks and values</h2>
     <rzslider rz-slider-model="slider_ticks_values.value" rz-slider-options="slider_ticks_values.options"></rzslider>
-  </div>  
+  </div>
+  <br>
   <div class="slider">
     <h2>Range slider with ticks and values</h2>
     <rzslider rz-slider-model="range_slider_ticks_values.minValue" rz-slider-high="range_slider_ticks_values.maxValue" rz-slider-options="range_slider_ticks_values.options"></rzslider>
