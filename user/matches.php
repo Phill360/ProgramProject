bf929db1ad53c5e95e2f7e5ab6f5321dfb5c5d3a
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-25 11:28:31 +0000

    db

--- user/matches.php
+++ user/matches.php
@@ -11,7 +11,7 @@
 	}
 ?>
 
-<!--<!DOCTYPE html PUBLIC>
+<DOCTYPE html PUBLIC>
 <html lang="en">
 <head>
   <title>Paw Companions</title>
@@ -20,38 +20,38 @@
 </head>
 
 <body>
-
-  <?php
-    // While loop fetches pets from the 'animals' table
-    while($row = mysqli_fetch_assoc($result)) {
-  ?>
-      <div class="col-xs-6 col-sm-4">
-        <!--<div class="panel panel-default">-->
-        <!--  <div class="panel-heading">-->
-        <!--    <div class="opensans"><?php echo $row["petName"]; ?></div>-->
-        <!--  </div>-->
-        <!--  <div class="panel-body">-->
-            <div class="thumbnail">
-              <div class="caption">
-                <div class="right">
-                  <a class="btn btn-default btn-lg" href="#">
-                  <span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span></a>
-                  <br><br>
-                  <div class="holder">
-                    <img src="<?php echo $row["imagePath"]; ?>" alt "pet">
-                  </div>
-                  </button>
+<div class="row">
+  <div class="col-xs-6 col-sm-4">
+    <?php
+      // While loop fetches pets from the 'animals' table
+      while($row = mysqli_fetch_assoc($result)) {
+    ?>
+        <div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="opensans"><?php echo $row["petName"]; ?></div>
+        </div>
+        <div class="panel-body">
+          <div class="thumbnail">
+            <div class="caption">
+              <div class="right">
+                <a class="btn btn-default btn-lg" href="#">
+                <span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span></a>
+                <br><br>
+                <div class="holder">
+                  <img src="<?php echo $row["imagePath"]; ?>" alt "pet">
                 </div>
-                <p><?php echo $row["description"]; ?></p>
-                <?php echo "<a href='view.php?PetId={$row['rspcaID']}'> More </a>"; ?>
+                </button>
               </div>
+              <p><?php echo $row["description"]; ?></p>
+              <?php echo "<a href='view.php?PetId={$row['rspcaID']}'> More </a>"; ?>
             </div>
-        <!--  </div>-->
-        <!--</div>-->
+          </div>
+        </div>
       </div>
-  <?php
-  }
-  ?>
+    <?php
+      }
+    ?>
+  </div>
 </div>
     <!-- /.container -->
     <div class="center">
