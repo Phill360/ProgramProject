d56fc60848a8fc5ef5e846cb14ecaac03b8090e0
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-25 02:20:10 +0000

    update

--- index.php
+++ index.php
@@ -525,9 +525,9 @@
             </div>
           <!--</div>-->
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
+        <!--<div class="modal-footer">-->
+        <!--  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
+        <!--</div>-->
       </div>
     </div>
   </div>
