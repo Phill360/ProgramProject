9deb6906743d3cd00a3ba3237c80e5a983be039d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-26 08:23:53 +0000

    db

--- user/matches.php
+++ user/matches.php
@@ -6,7 +6,18 @@ $sql = "SELECT COUNT(*) FROM animals";
 $result = mysql_query($sql, $connection) or trigger_error("SQL", E_USER_ERROR);
 $r = mysql_fetch_row($result);
 $numrows = $r[0];
-
-echo($numrows);
-
 ?>
+
+<DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+  <title>Paw Companions</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<?php echo($numrows);?>
+</body>
+  
+</html>
