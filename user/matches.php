4846454896ee829506345de1f1128b422647e2f9
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-26 08:20:17 +0000

    db

--- user/matches.php
+++ user/matches.php
@@ -3,7 +3,7 @@
   
 // find out how many rows are in the table 
 $sql = "SELECT COUNT(*) FROM animals";
-$result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
+$result = mysql_query($sql, $connection) or trigger_error("SQL", E_USER_ERROR);
 $r = mysql_fetch_row($result);
 $numrows = $r[0];
 
