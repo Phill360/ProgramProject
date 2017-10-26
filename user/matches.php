656d7b7862966b45b36d09041eccd1ef55608119
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-26 08:17:01 +0000

    db

--- user/matches.php
+++ user/matches.php
@@ -1,75 +1,87 @@
 <?php
   require_once('./_php/connect.php');
   
-try {  
-  // Find out how many items are in the table
-  $total = $dbh->query('SELECT COUNT(*) FROM animals')->fetchColumn();
-  
-  echo('The total is: '.$total);
-    
-  // How many items to list per page
-  $limit = 20;
+// find out how many rows are in the table 
+$sql = "SELECT COUNT(*) FROM animals";
+$result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
+$r = mysql_fetch_row($result);
+$numrows = $r[0];
 
-  // How many pages will there be
-  $pages = ceil($total / $limit);
+// number of rows to show per page
+$rowsperpage = 10;
+// find out total pages
+$totalpages = ceil($numrows / $rowsperpage);
 
-  // What page are we currently on?
-  $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
-    'options' => array(
-    'default'   => 1,
-    'min_range' => 1,
-    ),
-  )));
-    
-  // Calculate the offset for the query
-  $offset = ($page - 1)  * $limit;
+// get the current page or set a default
+if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+   // cast var as int
+   $currentpage = (int) $_GET['currentpage'];
+} else {
+   // default page num
+   $currentpage = 1;
+} // end if
 
-  // Some information to display to the user
-  $start = $offset + 1;
-  $end = min(($offset + $limit), $total);
-  
-  // The "back" link
-  $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+// if current page is greater than total pages...
+if ($currentpage > $totalpages) {
+   // set current page to last page
+   $currentpage = $totalpages;
+} // end if
+// if current page is less than first page...
+if ($currentpage < 1) {
+   // set current page to first page
+   $currentpage = 1;
+} // end if
 
-  // The "forward" link
-  $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+// the offset of the list, based on current page 
+$offset = ($currentpage - 1) * $rowsperpage;
 
-  // Display the paging information
-  echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+// get the info from the db 
+$sql = "SELECT id, number FROM numbers LIMIT $offset, $rowsperpage";
+$result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
 
-  // Prepare the paged query
-  $stmt = $dbh->prepare('
-        SELECT
-            *
-        FROM
-            animals
-        LIMIT
-            :limit
-        OFFSET
-            :offset
-  ');
-  
-  // Bind the query params
-  $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
-  $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
-  $stmt->execute();
+// while there are rows to be fetched...
+while ($list = mysql_fetch_assoc($result)) {
+   echo("RSPCA ID: ".$row["rspcaID"]);
+} // end while
 
-  // Do we have any results?
-  if ($stmt->rowCount() > 0) {
-  // Define how we want to fetch the results
-  $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  $iterator = new IteratorIterator($stmt);
-  // Display the results
-        foreach ($iterator as $row) {
-            echo '<p>', $row['petName'], '</p>';
-        }
+/******  build the pagination links ******/
+// range of num links to show
+$range = 3;
 
-    } else {
-        echo '<p>No results could be displayed.</p>';
-    }
-} catch (Exception $e) {
-    echo '<p>', $e->getMessage(), '</p>';
-}  
-  
-  
-  
+// if not on page 1, don't show back links
+if ($currentpage > 1) {
+   // show << link to go back to page 1
+   echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a> ";
+   // get previous page num
+   $prevpage = $currentpage - 1;
+   // show < link to go back to 1 page
+   echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a> ";
+} // end if 
+
+// loop to show links to range of pages around current page
+for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
+   // if it's a valid page number...
+   if (($x > 0) && ($x <= $totalpages)) {
+      // if we're on current page...
+      if ($x == $currentpage) {
+         // 'highlight' it but don't make a link
+         echo " [<b>$x</b>] ";
+      // if not current page...
+      } else {
+         // make it a link
+         echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$x'>$x</a> ";
+      } // end else
+   } // end if 
+} // end for
+
+// if not on last page, show forward and last page links        
+if ($currentpage != $totalpages) {
+   // get next page
+   $nextpage = $currentpage + 1;
+    // echo forward link for next page 
+   echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$nextpage'>></a> ";
+   // echo forward link for lastpage
+   echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$totalpages'>>></a> ";
+} // end if
+/****** end build pagination links ******/
+?>
