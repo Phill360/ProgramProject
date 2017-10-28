eb7564d4d6c3948ec46d291de9463ad7b1d05d71
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 05:11:13 +0000

    db

---
+++ user/display_image.php
@@ -0,0 +1,23 @@
+
+<html>
+	
+	<body>
+	    <?php
+	    echo('Is this page loading?');
+	    include_once('./common.php');
+	    
+	    
+	    // replace 'testing' with the rspcaID of the pet you want to display
+		displayimage('mark4');
+
+		
+	    ?>
+	</body>
+</html>
+
+
+
+
+
+
+
