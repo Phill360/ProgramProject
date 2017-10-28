8c465c0ef32b58ebe524af308903fc7d5c401586
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 09:14:27 +0000

    db

--- common.php
+++ common.php
@@ -565,7 +565,14 @@ function debug_to_console($data) {
 	function numberAnimalsInDatabase()
 	{
 	  // Connect AWS MYSQL Server
-    require_once('./_php/connect.php');
+    $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $DBpassword="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
 	  
 	  // Get number of pets in database
 	  $query = "SELECT * FROM animals";
