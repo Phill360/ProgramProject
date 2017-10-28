432633f09823f5d3b9483f51b0efec443ea32e0b
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 23:20:37 +0000

    update

--- common.php
+++ common.php
@@ -573,8 +573,14 @@ function debug_to_console($data) {
 	function fetchAnimalsFromDatabase($page1)
 	{
     // Connect AWS MYSQL Server
-    // Test for query error ////////////////////////////////////////
-    $new_id = mysqli_insert_id($connection);
+    $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $DBpassword="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
     
 	  $query = "SELECT * FROM animals LIMIT $page1,12"; 
 	  $result = mysqli_query($connection, $query);
