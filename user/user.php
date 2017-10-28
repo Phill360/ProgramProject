81e27e7e707df8e3bf19057a41281ff7e13f9761
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 05:26:20 +0000

    db

--- user/user.php
+++ user/user.php
@@ -95,22 +95,8 @@
   </div>
 
   <?php
-    if ($_SESSION['userTool'] == 'questionnaire')
-    {?>
-      <div><?php include 'questionnaire.php' ?></div>
-    <?php }
-    else if ($_SESSION['userTool'] == 'matches')
-    {?>
-      <div><?php include 'display_image.php' ?></div> 
-    <?php } 
-    else if ($_SESSION['userTool'] == 'favourites')
-    {?>
-      <div><?php include 'favourites.php' ?></div>
-    <?php }
-    else
-    {?>
-      <div><?php include 'matches.php' ?></div>
-  <?php } ?>
+    include 'display_image.php' 
+  ?>
 </div>
 
 </body>
