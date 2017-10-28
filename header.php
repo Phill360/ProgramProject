7c5978f68f91a81e5442f4062fb9e925de3897b8
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 22:44:24 +0000

    update

--- header.php
+++ header.php
@@ -21,6 +21,7 @@ $status = checkStatus();
       {?>
         <button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#signInModal">Sign in</button>
         <button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#registerModal">Register</button>
+        <br><div class="opensans"><div class="black"><div class="textSmall"><?php echo("Welcome ".$_SESSION[email]);?></div></div></div>
       <?php
       }
       else
