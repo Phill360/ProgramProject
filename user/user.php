6c21c55673cc31d05a9f198415dea582bfea1443
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 07:09:05 +0000

    db

--- user/user.php
+++ user/user.php
@@ -1,8 +1,8 @@
 <?php
   include_once('./common.php');
 
-  $number = checkNumberUsersInFile();
   setupUserSession();
+  $number = checkNumberUsersInFile();
 
   if(isset($_POST['questionnaireBtn']))
   {
