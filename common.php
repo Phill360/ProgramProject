bb857e03ef7db78d9106aaaf6d524ac61b3ba831
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 09:01:13 +0000

    db

--- common.php
+++ common.php
@@ -474,7 +474,7 @@ function debug_to_console($data) {
 	function setupUserSession()
 	{
 	  // Connect AWS MYSQL Server
-    require_once('./_php/connect.php');
+    require('./_php/connect.php');
   
     $email = $_SESSION['email'];
   
@@ -503,14 +503,7 @@ function debug_to_console($data) {
 	  
 	  mysqli_close($connection);
 	  
-	  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
-    $port=3306;
-    $socket="";
-    $user="proProg";
-    $DBpassword="pawprogramming";
-    $dbname="pawCompanion";
-    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
-    	or die ('Could not connect to the database server' . mysqli_connect_error());
+	  
 	  
 	  
 	  
