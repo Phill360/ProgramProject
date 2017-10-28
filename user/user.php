7ccc806fbe329b8c4408b62120e20fdcdf8d77c3
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 05:34:30 +0000

    db

--- user/user.php
+++ user/user.php
@@ -85,35 +85,7 @@
 </head>
 
 <body>
-  <div><?php include 'display_image.php' ?></div>
-<div class="container">
-  
-  <div>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" id="userToolSelectionForm">
-      <button type="submit" class="btn" name="questionnaireBtn">Questionnaire</button>
-      <button type="submit" class="btn" name="matchesBtn">Matches</button>
-      <button type="submit" class="btn" name="favouritesBtn">Favourites</button>
-    </form>
-  </div>
-
-  <?php
-    if ($_SESSION['userTool'] == 'questionnaire')
-    {?>
-      <div><?php include 'questionnaire.php' ?></div>
-    <?php }
-    else if ($_SESSION['userTool'] == 'matches')
-    {?>
-      <div><?php include 'display_image.php' ?></div> 
-    <?php } 
-    else if ($_SESSION['userTool'] == 'favourites')
-    {?>
-      <div><?php include 'favourites.php' ?></div>
-    <?php }
-    else
-    {?>
-      <div><?php include 'matches.php' ?></div>
-  <?php } ?>
-</div>
+<div><?php include 'display_image.php' ?></div>
 
 </body>
 </html>
