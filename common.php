c6c50c6ed2bbe0b9d042314f1fe5cede133c9dfd
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 08:56:09 +0000

    db

--- common.php
+++ common.php
@@ -502,8 +502,18 @@ function debug_to_console($data) {
 	  }
 	  
 	  mysqli_close($connection);
-	  mysqli_init();
-	  require_once('./_php/connect.php');
+	  
+	  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $DBpassword="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
+	  
+	  
+	  
 	
 	  /* With the userID we now check if this user has visited the site previously. */
       
