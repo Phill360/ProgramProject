a2ca33b946ab650cb84cf14962c815e294629d94
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 09:06:25 +0000

    db

--- common.php
+++ common.php
@@ -503,14 +503,7 @@ function debug_to_console($data) {
 	  
 	  mysqli_close($connection);
 	  
-	  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
-    $port=3306;
-    $socket="";
-    $user="proProg";
-    $DBpassword="pawprogramming";
-    $dbname="pawCompanion";
-    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
-    	or die ('Could not connect to the database server' . mysqli_connect_error());
+	  include(_php/connect.php);
 	  
 	  
 	  
