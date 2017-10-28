7a1422a370709a88f947eae82c72acbc2bf1546d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-10-28 07:32:32 +0000

    db

--- user/matches.php
+++ user/matches.php
@@ -26,14 +26,15 @@
   }
     
   //$result = fetchAnimalsFromDatabase($page1);
+  require_once('./_php/connect.php');
   $query = "SELECT * FROM animals LIMIT $page1,12"; 
-	  $result = mysqli_query($connection, $query);
+	$result = mysqli_query($connection, $query);
 	  
-	  // Test for query error
-	  if(!$result) 
-	  {
+	// Test for query error
+	if(!$result) 
+	{
 		  die("7. Database query failed.");
-	  }
+	}
   
   
   
