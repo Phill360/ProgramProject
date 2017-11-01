b193d30c76d06fbcba287a363ac7b274a7381cff
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-01 12:07:47 +0000

    update

--- user/fav.php
+++ user/fav.php
@@ -1,5 +1,7 @@
 <?php
 
+  session_start();
+  
   function is_ajax_request() 
   {
     return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
@@ -8,12 +10,13 @@
 
   if(!is_ajax_request()) { exit; }
 
-  // extract $id
-  $animalID = isset($_POST['id']) ? $_POST['id'] : '';
-  //echo $animalID;
-  
+  // Get the current user
   $userID = $_SESSION[email];
+  
+  // Extract $id
+  $animalID = isset($_POST['id']) ? $_POST['id'] : '';
   echo $userID;
+  
   //addPetToFavourites($userID, $animalID);
   
 ?>
