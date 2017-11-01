588e5d7b68b1e1071aa43ea680b74ed6a8cb420e
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-01 10:42:01 +0000

    db

---
+++ user/fav.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+
+  function is_ajax_request() 
+  {
+      return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+  }
+  
+  if(!is_ajax_request())
+  {
+      exit;
+  }
+  
+  $raw_id = isset($_POST['id']) ? $_POST['id'] : '';
+  echo $raw_id;
+?>
