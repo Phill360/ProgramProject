f0fafd2ccb656060ee1f3eb5291abd0211d30d39
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-01 09:46:04 +0000

    update

--- user/matches.php
+++ user/matches.php
@@ -30,7 +30,7 @@
   // Fetch pets from the 'animals' table
   while($row = mysqli_fetch_assoc($result)) {
   ?>
-    <div class="col-xs-12 col-sm-6 col-md-4">
+    <div id=<?php echo($row["rspcaID"]); ?> class="col-xs-12 col-sm-6 col-md-4">
         <div class="panel panel-default">
           <div class="panel-heading">
             <div class="opensans"><?php echo("RSPCA ID: ".$row["rspcaID"]); ?></div>
