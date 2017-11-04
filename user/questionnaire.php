2f75eb2b9dd7c435bbc4e41d2a8f2f39c924f355
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-04 04:03:42 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -147,15 +147,15 @@
         <div class="panel-body">
           <div class="panel-heading">Gender preference?
 
-
+            
               <div class="btn-group" data-toggle="buttons">
-                <label class="btn btn-primary active">
+                <label class="btn btn-md btn-primary active">
                   <input type="radio" name="petGender" id="petGender" value=1 autocomplete="off" checked> Female
                 </label>
-                <label class="btn btn-primary">
+                <label class="btn btn-md btn-primary">
                   <input type="radio" name="petGender" id="petGender" value=2 autocomplete="off"> Male
                 </label>
-                <label class="btn btn-primary">
+                <label class="btn btn-md btn-primary">
                   <input type="radio" name="petGender" id="petGender" value=3 autocomplete="off"> Don't care
                 </label>
               </div>
