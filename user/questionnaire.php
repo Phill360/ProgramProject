eb3d64fcc232ff59c29117f9f8270d18efafc094
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-04 04:29:53 +0000

    update

--- user/questionnaire.php
+++ user/questionnaire.php
@@ -65,18 +65,17 @@
       
       <div class="panel panel-default">
         <div class="panel-body">
-          <div class="panel-heading">What type of animal do you prefer?
-            <div class="btn-group" data-toggle="buttons">
-              <label class="btn btn-md btn-primary active">
-                <input type="radio" name="petSelection" id="petSelection" value=1 autocomplete="off" checked> Cat
-              </label>
-              <label class="btn btn-md btn-primary">
-                <input type="radio" name="petSelection" id="petSelection" value=2 autocomplete="off"> Dog
-              </label>
-              <label class="btn btn-md btn-primary">
-                <input type="radio" name="petSelection" id="petSelection" value=3 autocomplete="off"> Don't care
-              </label>
-            </div>
+          <div class="panel-heading">What type of animal do you prefer?</div>
+          <div class="btn-group" data-toggle="buttons">
+            <label class="btn btn-md btn-primary active">
+              <input type="radio" name="petSelection" id="petSelection" value=1 autocomplete="off" checked> Cat
+            </label>
+            <label class="btn btn-md btn-primary">
+              <input type="radio" name="petSelection" id="petSelection" value=2 autocomplete="off"> Dog
+            </label>
+            <label class="btn btn-md btn-primary">
+              <input type="radio" name="petSelection" id="petSelection" value=3 autocomplete="off"> Don't care
+            </label>
           </div>
         </div>
       </div>
@@ -105,19 +104,18 @@
       
       <div class="panel panel-default">
         <div class="panel-body">
-          <div class="panel-heading">Gender preference?
-            <div class="btn-group" data-toggle="buttons">
-              <label class="btn btn-md btn-primary active">
-                <input type="radio" name="petGender" id="petGender" value=1 autocomplete="off" checked> Female
-              </label>
-              <label class="btn btn-md btn-primary">
-                <input type="radio" name="petGender" id="petGender" value=2 autocomplete="off"> Male
-              </label>
-              <label class="btn btn-md btn-primary">
-                <input type="radio" name="petGender" id="petGender" value=3 autocomplete="off"> Don't care
-              </label>
-            </div>
-	        </div>  
+          <div class="panel-heading">Gender preference?</div>
+          <div class="btn-group" data-toggle="buttons">
+            <label class="btn btn-md btn-primary active">
+              <input type="radio" name="petGender" id="petGender" value=1 autocomplete="off" checked> Female
+            </label>
+            <label class="btn btn-md btn-primary">
+              <input type="radio" name="petGender" id="petGender" value=2 autocomplete="off"> Male
+            </label>
+            <label class="btn btn-md btn-primary">
+              <input type="radio" name="petGender" id="petGender" value=3 autocomplete="off"> Don't care
+            </label>
+          </div>
         </div>
       </div>
       
