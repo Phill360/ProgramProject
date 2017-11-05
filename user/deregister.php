1836e19fbbfe81ed6d357918fee6946783f57428
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-05 09:40:37 +0000

    update

--- user/deregister.php
+++ user/deregister.php
@@ -12,6 +12,11 @@
 <body>
 <!-- Description -->  
 <div class="container">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="panel panel-default">
+          <div class="textMedium"><div class="opensans">Are you sure you wish to deregister yourself from Pet Companions?</div></div>
+        </div>
+    </div>
     <div class="textMedium"><div class="opensans">Are you sure you wish to deregister yourself from Pet Companions?</div></div>
     <br>
 </div>
