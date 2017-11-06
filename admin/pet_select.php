2c45e36058819dc33e25e3a96a6382eafd2036eb
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-06 10:55:00 +0000

    db

--- admin/pet_select.php
+++ admin/pet_select.php
@@ -51,7 +51,7 @@
               <tr>
 				        <td><?php echo $row["rspcaID"] ; ?> </td> 
 				        <td><?php echo $row["petName"] ; ?>	</td>
-				        <td><button type="submit" class="btn btn-success" name="editPetBtn" value={$row['rspcaID']}>Edit</button></td>
+				        <td><button type="submit" class="btn btn-success" name="editPetBtn" value=$row['rspcaID']>Edit</button></td>
 				        <td><button type="submit" class="btn btn-danger" name="removePetBtn" value={$row['rspcaID']}>Remove</button></td>
 			        </tr>
               <?php
