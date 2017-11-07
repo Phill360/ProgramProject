a920ed0efc2b95ec90d6b97916b9b8746dfa0f4d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-07 23:54:06 +0000

    db

---
+++ admin/breed_edit.php
@@ -0,0 +1,63 @@
+<?php
+
+
+  
+?>
+
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+
+</head>
+
+<body>
+
+  <div class="row">
+    <div class="col-sm-12">
+      <!-- Remove a breed box -->
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="opensans">Remove a breed</div>
+        </div>
+        <div class="panel-body">
+          
+          <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">
+            <div class="input-group">
+              <span class="input-group-addon">Breed Name</span>
+              <!--<input id="breedID" type="text" class="form-control" name="breedID" placeholder="Enter breed ID">-->
+                <select class="form-control" id="species" name="breedID" required>
+            
+                  <?php
+                  // Connect AWS MYSQL Server
+                  require('./_php/connect.php');
+  
+  	              // 2. Perform Query
+  	              $query = "SELECT breedID, name ";
+  	              $query .= "FROM breed ";
+  	              $result = mysqli_query($connection, $query);
+                	// Test for query error
+                	if(!$result) {
+                		die("Database query failed.");
+                	}
+  	
+  	
+                  // Generate Breed List
+                  while($row = mysqli_fetch_assoc($result)) {
+                      echo "<option value=\"" . $row["breedID"] . "\">" . $row["name"] . "</option>" ;
+                  } 
+                   mysqli_close($connection);
+                  ?>
+            
+                </select>
+              </div>
+            <br>
+            <button type="submit" class="btn btn-primary" name="remBreedBtn">Remove</button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</body>
+</html>
---
+++ admin/breed_select.php
@@ -0,0 +1,73 @@
+<?php
+?>
+
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+<head>
+</head>
+
+<body>
+
+  <div class="row">
+    <div class="col-sm-12">
+      <!-- Edit/Remove table -->
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="opensans">Edit or remove a breed</div>
+        </div>
+        <div class="panel-body">
+          <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post" enctype="multipart/form-data">
+            
+            <?php
+            // Connect AWS MYSQL Server
+            require('_php/connect.php');
+
+	          $query = "SELECT * FROM breed";
+	          $result = mysqli_query($connection, $query);
+            
+            // Test for query error
+            if(!$result) 
+            {
+              die("Database query failed.");
+            }
+	          
+	          ?>
+	          <table class="table table-hover">
+	            <thead>
+                <tr>
+                  <th class="col-xs-3 col-sm-3 col-md-2 col-lg-2">ID</th>
+                  <th class="col-xs-3 col-sm-3 col-md-2 col-lg-2">Bredd</th>
+                  <th class="col-xs-3 col-sm-3 col-md-2 col-lg-2">Edit</th>
+                  <th class="col-xs-3 col-sm-3 col-md-2 col-lg-2">Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+	          <?php
+	          
+            // List animals in database
+            while($row = mysqli_fetch_assoc($result)) 
+            {
+              ?>
+              <tr>
+				        <td><?php echo $row["breedID"] ; ?> </td> 
+				        <td><?php echo $row["breedName"] ; ?>	</td>
+				        <td><button type="submit" class="btn btn-success" name="editBreedBtn" value=<?php echo $row["breedID"]?>>Edit</button></td>
+				        <td><button type="submit" class="btn btn-danger" name="removeBreedBtn" value=<?php echo $row["breedID"]?>>Remove</button></td>
+			        </tr>
+              <?php
+            }
+            ?>
+            </tbody>
+            </table>
+            <?php
+            
+            mysqli_close($connection);
+            ?>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
