a0e7c21c30ae92293e273fd76efc9f183ee8cc04
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 04:21:56 +0000

    update

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -7,6 +7,11 @@
 </head>
 
 <body>
+  <div class='device-check visible-xs' data-device='xs'></div>
+  <div class='device-check visible-sm' data-device='sm'></div>
+  <div class='device-check visible-md' data-device='md'></div>
+  <div class='device-check visible-lg' data-device='lg'></div>
+  <?php echo(getCurrentGridOption()); ?>
 
   <div class="row">
     <div class="col-sm-12">
@@ -82,5 +87,11 @@
       </div>
     </div>
   </div>
+  <script>
+    function getCurrentGridOption()
+    {
+      return $('.device-check:visible').attr('data-device')
+    }
+  </script>
 </body>
 </html>
