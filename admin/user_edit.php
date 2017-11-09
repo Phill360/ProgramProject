f44dbfee5983c3b7a896031219011d8e80de2127
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 04:40:15 +0000

    db

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -7,11 +7,6 @@
 </head>
 
 <body>
-  <div class='device-check visible-xs' data-device='xs'></div>
-  <div class='device-check visible-sm' data-device='sm'></div>
-  <div class='device-check visible-md' data-device='md'></div>
-  <div class='device-check visible-lg' data-device='lg'></div>
-
   <div class="row">
     <div class="col-sm-12">
       <!-- Remove table -->
@@ -86,14 +81,5 @@
       </div>
     </div>
   </div>
-  <?php
-    function getCurrentGridOption()
-    {
-      return $('.device-check:visible').attr('data-device')
-    }
-    
-    $screen = getCurrentGridOption();
-    echo $screen;
-  ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -585,6 +585,19 @@
     </div>
 <br>
 <div><?php include 'footer.php' ?></div>
+
+<div class='device-check visible-xs' data-device='xs'></div>
+<div class='device-check visible-sm' data-device='sm'></div>
+<div class='device-check visible-md' data-device='md'></div>
+<div class='device-check visible-lg' data-device='lg'></div>
+<?php
+  function get_current_grid_option()
+  {
+    $answer = ('.device-check:visible').attr('data-device');
+    return $answer;
+  }
+  echo $answer;
+?>
 </body>
 </html>
 
