2a72f0d6e50e14a7326b3749cfcd6ce482ec0e09
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 05:39:22 +0000

    db

--- common.php
+++ common.php
@@ -283,6 +283,8 @@ function getUserID($email)
 	$query = "SELECT userID FROM user WHERE email = '" . $email . "'";
 	$result = mysqli_query($connection, $query);
 	
+	echo $resut;
+	
 	// Test for query error
 	if(!$result) 
 	{
