fca4711954f228479bd0e19cb0a6de5f6a7f9d1d
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 12:22:02 +0000

    update

--- admin/admin.php
+++ admin/admin.php
@@ -52,13 +52,46 @@ include_once('./common.php');
       <button type="submit" class="btn" name="editRemoveBreedBtn">Edit or remove a breed</button>
       <button type="submit" class="btn" name="editUserBtn">Edit or remove a user</button>
     </form>
-    
-    
-		
 	</div>
-    
-
-
+  <div>
+    <div class="col-md-6">
+      <div class="input-group" id="adv-search">
+        <input type="text" class="form-control" placeholder="Search for snippets" />
+        <div class="input-group-btn">
+          <div class="btn-group" role="group">
+            <div class="dropdown dropdown-lg">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
+              <div class="dropdown-menu dropdown-menu-right" role="menu">
+                <form class="form-horizontal" role="form">
+                  <div class="form-group">
+                    <label for="filter">Filter by</label>
+                    <select class="form-control">
+                      <option value="0" selected>All Snippets</option>
+                      <option value="1">Featured</option>
+                      <option value="2">Most popular</option>
+                      <option value="3">Top rated</option>
+                      <option value="4">Most commented</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="contain">Author</label>
+                    <input class="form-control" type="text" />
+                  </div>
+                  <div class="form-group">
+                    <label for="contain">Contains the words</label>
+                    <input class="form-control" type="text" />
+                  </div>
+                  <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                </form>
+              </div>
+            </div>
+            <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
   <?php 
     if ($adminTool == 'editUser')
     {?>
