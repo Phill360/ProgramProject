2b0583692fe99cd1a02f1d6a04d586cc85826cc2
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 02:57:27 +0000

    update

--- common.php
+++ common.php
@@ -257,6 +257,7 @@ function checkUserType()
 function getUserType($userID)
 {
 	include_once('_php/connect.php');
+	echo $userID;
 	
 	$query = "SELECT userType FROM user WHERE userID = $userID";
 	$result = mysqli_query($connection, $query);
