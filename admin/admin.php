6fe927b3b98edf4167888c87c2e383724abfa486
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-09 21:18:29 +0000

    update

--- admin/admin.php
+++ admin/admin.php
@@ -54,32 +54,6 @@ include_once('./common.php');
     </form>
 	</div>
   
-  <div class="row">
-    <div class="col-sm-12">
-      <!-- Search box -->
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <div class="opensans">Search</div>
-        </div>
-        <div class="panel-body">
-          <div class="container">
-	          <div class="row">
-              <div id="custom-search-input">
-                <div class="input-group col-md-6">
-                  <input type="text" class="  search-query form-control" placeholder="Search" />
-                    <span class="input-group-btn">
-                    <button class="btn btn-primary" type="button">
-                      <span class=" glyphicon glyphicon-search"></span>
-                    </button>
-                  </span>
-                </div>
-              </div>
-	          </div>
-          </div>
-        </div>
-      </div>  
-    </div>
-  </div>  
   <?php 
     if ($adminTool == 'editUser')
     {?>
