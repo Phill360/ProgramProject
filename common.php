24068131db6637fc3c88f2211eb8568d94875c0f
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-10 00:24:40 +0000

    db

--- common.php
+++ common.php
@@ -909,7 +909,7 @@ function getLimitedNumberOfAnimalsFromDatabase($page1)
 	// Test for query error
 	if(!$result) 
 	{
-		die("7. Database query failed.");
+		die("Database query failed. getLimitedNumberOfAnimalsFromDatabase()");
 	}
 	  
 	return $result;
@@ -920,7 +920,17 @@ function getLimitedNumberOfAnimalsFromDatabase($page1)
 function getNumberOfAnimals()
 {
 	// Connect AWS MYSQL Server
-  require_once('./_php/connect.php');
+  // require_once('./_php/connect.php');
+  
+  // Connect AWS MYSQL Server
+  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+  $port=3306;
+  $socket="";
+  $user="proProg";
+  $DBpassword="pawprogramming";
+  $dbname="pawCompanion";
+  $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
   
   // Perform query
   $query = "SELECT COUNT(*) FROM animals";
