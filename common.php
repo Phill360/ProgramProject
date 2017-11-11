ad04808fbbebaa5ab40c3fea27205ba59db313a7
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-11 04:42:07 +0000

    db

--- common.php
+++ common.php
@@ -446,7 +446,15 @@ Cats also match to extra small. According to Google adult cats weigh 3.6 - 4.5kg
 function submitResponses($adultsHome, $childrenHome, $howActive, $howOftenHome, $petSelection, $petSize, $petTemperament, $petGender)
 {
   // Connect AWS MYSQL Server
-  require_once('./_php/connect.php');
+  //require_once('./_php/connect.php');
+  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $DBpassword="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
   
   // Insert search data into userSearch Table
 	$query = "INSERT INTO userSearch ";
