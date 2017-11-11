3a7bce936a75dfd7d6c77a2c609d8220b172be8e
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-11 04:11:25 +0000

    db

--- common.php
+++ common.php
@@ -80,14 +80,14 @@ function registerUser($firstname, $lastname, $email, $password)
     $_SESSION['firstName'] = $firstname;
     $_SESSION['lastName'] = $lastname;
     
+    // Close database connection
+    mysqli_close($connection);
+    
     $userID = getUserID($email);
     $_SESSION['userID'] = $userID;
     
     header('Location: index.php');
   }
-  
-  // Close database connection
-  mysqli_close($connection);
 }
 
 /* This function signs the user in */
