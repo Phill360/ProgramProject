f73cc03067e0bfd3c8d454f330f11eedb871ac3a
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-11 04:31:58 +0000

    db

--- common.php
+++ common.php
@@ -298,7 +298,15 @@ function getUserType($userID)
 /* This function gets the userID from email address */
 function getUserID($email)
 {
-	include('connect.php');
+	// include('connect.php');
+	$host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+    $port=3306;
+    $socket="";
+    $user="proProg";
+    $DBpassword="pawprogramming";
+    $dbname="pawCompanion";
+    $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    	or die ('Could not connect to the database server' . mysqli_connect_error());
 	
 	$query = "SELECT userType FROM user WHERE email = '" . $email . "'";
 	$result = mysqli_query($connection, $query);
