5023bb8e21bcd986d0f3e941569c5ce9b7536b61
Phillip Attard <s3472625@student.rmit.edu.au>
2017-11-14 09:17:00 +0000

    db

--- common.php
+++ common.php
@@ -1071,16 +1071,16 @@ function displayImage($rspcaID)
 function getAnimalName($rspcaID)
 {
 	// Connect AWS MYSQL Server
-	require_once('./_php/connect.php');
+	// require_once('./_php/connect.php');
 	
-  // $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
-  // $port=3306;
-  // $socket="";
-  // $user="proProg";
-  // $DBpassword="pawprogramming";
-  // $dbname="pawCompanion";
-  // $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
-  //   or die ('Could not connect to the database server' . mysqli_connect_error());
+  $host="petdatabase.colkfztcejwd.us-east-2.rds.amazonaws.com";
+  $port=3306;
+  $socket="";
+  $user="proProg";
+  $DBpassword="pawprogramming";
+  $dbname="pawCompanion";
+  $connection = new mysqli($host, $user, $DBpassword, $dbname, $port, $socket)
+    or die ('Could not connect to the database server' . mysqli_connect_error());
     	
   // Get favourites
 	$query = "SELECT * FROM animals";
